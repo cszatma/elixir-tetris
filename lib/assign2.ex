607765aa4e8591877f6ce8a55236ddbb8c5b0160
Chris-Bishop <christopher.allan.bishop@gmail.com>
2017-10-17 09:22:43 -0400

    Initial Commit

---
+++ lib/assign2.ex
@@ -0,0 +1,18 @@
+defmodule Assign2 do
+  @moduledoc """
+  Documentation for Assign2.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Assign2.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/assign2_test.exs
@@ -0,0 +1,8 @@
+defmodule Assign2Test do
+  use ExUnit.Case
+  doctest Assign2
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
