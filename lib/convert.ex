cef5755fe40533f82a0774adad0c2f3a438f49f0
Chris-Bishop <christopher.allan.bishop@gmail.com>
2017-10-20 11:24:02 -0400

    Set default task mix task to test case

--- lib/convert.ex
+++ lib/convert.ex
@@ -15,18 +15,18 @@ defmodule Convert do
     rowsToString(board, numRows, numCols, "", numRows)
   end
 
-  
+
   def rowsToString(board, numRows, numCols, _, _) do
     newLine = colsToString(board, numRows, 1, numCols, "")
     result = newLine <> " This is the top row!\n"
-    rowsToString(board, numRows - 1, numCols, result) 
+    rowsToString(board, numRows - 1, numCols, result)
   end
 
 
   def rowsToString(board, numRows, numCols, result) when numRows > 0 do
     newLine = colsToString(board, numRows, 1, numCols, "")
     result = result <> newLine <> "\n"
-    rowsToString(board, numRows - 1, numCols, result) 
+    rowsToString(board, numRows - 1, numCols, result)
   end
 
   def rowsToString(board, 0, _, result) do
@@ -37,7 +37,7 @@ defmodule Convert do
 
 
   def colsToString(board, numRows, currentCol, maxCols, line) when currentCol <= maxCols do
-    line = line <> board[numRows][currentCol] 
+    line = line <> board[numRows][currentCol]
     colsToString(board, numRows, currentCol + 1, maxCols, line)
   end
 
@@ -53,18 +53,18 @@ defmodule Convert do
   def colsToString(_, line) do
   "+" <> line <> "+"
   end
-  
+
   # Converts a map to a list
   def mapMatrixToListMatrix(mapMatrix) do
     Map.values(mapMatrix)
     |> Enum.map(&Map.values(&1))
   end
-  
+
   # Converts a list to a map
   def listMatrixToMapMatrix(listMatrix) do
     vectorsToMap(%{}, listMatrix, 1)
   end
-  
+
   # Converts a given 1d list (vector) to a map and adds
   # it to a given map with the given index as the key
   def addVectorToMap(map, vector, index) do
